4cc1148be4829878ec9cbc4f2f009d324feadb1d
Priyanka Ravi <priyanka.ravi101@gmail.com>
2023-05-08 13:12:22 -0700

    Add subnet_ids output
    Signed-off-by: Priyanka Ravi <priyanka.ravi101@gmail.com>

--- terraform/cluster-templates/core/outputs.tf
+++ terraform/cluster-templates/core/outputs.tf
@@ -5,3 +5,7 @@ output "public_subnets" {
 output "private_subnets" {
   value       = join(",", module.vpc.private_subnets)
 }
+
+output "subnet_ids" {
+  value = join(",", concat(module.vpc.public_subnets, module.vpc.private_subnets))
+}
